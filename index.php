92dcd01222001ec6c9244e6223dda5ec6bbfb3fc
Nathan Lepron <48675531+nathanlepron@users.noreply.github.com>
2023-01-30 22:55:42 +0100

    release

--- index.php
+++ index.php
@@ -1,37 +1,19 @@
 <?php
-    $kv_username = "" .getenv('usernamepostgre')."@pg-srv-nathan-lepron";
-    $kv_password = getenv('passwordpostgre');
-   class Database {
-       private $host = "pg-srv-nathan-lepron.postgres.database.azure.com";
-       private $db_name = "pg-db-nathan-lepron";
-       public $conn;
-
-       public function getConnection($username,$password) {
-           $this->conn = null;
-
-           try {
-               $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->db_name, $username, $password);
-           } catch(PDOException $exception) {
-               echo "Connection error: " . $exception->getMessage() . "/" . $password . "/" . $username;
-           }
-
-           return $this->conn;
-       }
-       public function readAllCategories() {
-        $sql = "SELECT * FROM category";
- 
-        $stmt = $this->conn->prepare($sql);
-        $stmt->execute();
- 
-        return $stmt;
+    $host = "pg-srv-nathan-lepron.postgres.database.azure.com";
+    $db_name = "pg-db-nathan-lepron";
+    $username = "" . $_ENV['usernamepostgre'] . "@pg-srv-nathan-lepron";
+    $password = "".$_ENV['passwordpostgre']."";
+    try {
+        $conn = new PDO("pgsql:host=" . $host . ";dbname=" . $db_name, $username, $password);
+    } catch(PDOException $exception) {
+        echo "Connection error: " . $exception->getMessage();
     }
-   }
-   $database = new Database();
-   $conn = $database->getConnection($kv_username,$kv_password);
-   $stmt = $database->readAllCategories();
-   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header('Content-Type: application/json');
-echo json_encode($rows);
+    $sql = "SELECT * FROM category";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    header('Content-Type: application/json');
+    echo json_encode($rows);
 ?>
