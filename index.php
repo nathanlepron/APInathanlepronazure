c085b2012d7849ca8d7e345cced21afb9b365a6b
Nathan Lepron <48675531+nathanlepron@users.noreply.github.com>
2023-01-30 14:46:31 +0100

    Pushing 3

--- index.php
+++ index.php
@@ -1,39 +1,38 @@
 <?php
     $kv_username = "" .$_ENV['usernamepostgre']."@pg-srv-nathan-lepron";
     $kv_password = $_ENV['passwordpostgre'];
-//   class Database {
-//       private $host = "pg-srv-nathan-lepron.postgres.database.azure.com";
-//       private $db_name = "pg-db-nathan-lepron";
-//       public $conn;
-//
-//       public function getConnection() {
-//           $this->conn = null;
-//
-//           try {
-//               $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->db_name, $kv_username, $kv_password);
-//           } catch(PDOException $exception) {
-//               echo "Connection error: " . $exception->getMessage();
-//           }
-//
-//           return $this->conn;
-//       }
-//       public function readAllCategories() {
-//        $sql = "SELECT * FROM category";
-// 
-//        $stmt = $this->conn->prepare($sql);
-//        $stmt->execute();
-// 
-//        return $stmt;
-//    }
-//   }
-//   $database = new Database();
-//   $conn = $database->getConnection();
-//   $stmt = $database->readAllCategories();
-//   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo $kv_password;
-echo $kv_username;
-//header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Headers: *");
-//header('Content-Type: application/json');
-//echo json_encode($rows);
+   class Database {
+       private $host = "pg-srv-nathan-lepron.postgres.database.azure.com";
+       private $db_name = "pg-db-nathan-lepron";
+       public $conn;
+
+       public function getConnection() {
+           $this->conn = null;
+
+           try {
+               $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->db_name, $kv_username, $kv_password);
+           } catch(PDOException $exception) {
+               echo "Connection error: " . $exception->getMessage();
+           }
+
+           return $this->conn;
+       }
+       public function readAllCategories() {
+        $sql = "SELECT * FROM category";
+ 
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+ 
+        return $stmt;
+    }
+   }
+   $database = new Database();
+   $conn = $database->getConnection();
+   $stmt = $database->readAllCategories();
+   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header('Content-Type: application/json');
+echo json_encode($rows);
 ?>
